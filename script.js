e55d524382a05d332675b366f9a3f21a801fff22
dejan <dejan.petrusevic184@gmail.com>
2022-04-29 13:32:05 +0200

    Edit script.js switch statement

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ gameGrid.append(computerChoiceDisplay, userChoiceDisplay, resultDisplay)
 
 const choices = ['rock', 'paper', 'scissors']
 let userChoice
+let computerCoices
 
 const handClick = (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = 'User choice: ' + userChoice
     generateComputerChoice()
+    getResult()
 }
 
 const generateComputerChoice = () => {
     const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+    computerCoices = randomChoice
     computerChoiceDisplay.innerHTML = 'Computer choice: ' + randomChoice
 }
 
@@ -24,4 +27,24 @@ for(let i = 0; i < choices.length; i++){
     button.innerHTML = choices[i]
     button.addEventListener('click', handClick)
     gameGrid.appendChild(button)
+}
+
+const getResult = () => {
+    switch (computerCoices + userChoice){
+        case 'scissorspaper':
+        case 'rockscissors':
+        case 'paperrock':
+            resultDisplay.innerHTML = 'YOU WIN!'
+            break
+        case 'paperscissors':
+        case 'scissorsrock':
+        case 'rockpaper':
+            resultDisplay.innerHTML = 'YOU LOSE!'
+            break
+        case 'paperpaper':
+        case 'scissorsscissors':
+        case 'rockrock':
+            resultDisplay.innerHTML = 'ITS A DRAW!'
+            break
+    }
 }
